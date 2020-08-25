df9a6f504b4e5afcd746c16f388932e54838e711
James Auman <jtauman@nc.rr.com>
2020-08-25 10:24:15 -0400

    adding sql queries, query data, query screenshot

--- queries.sql
+++ queries.sql
@@ -1,23 +1,53 @@
---Create tables for raw data to be loaded into food_test_db
+--Create tables for raw data to be loaded into restaurant_db
 CREATE TABLE restaurant (
-id TEXT PRIMARY KEY,
-city TEXT,
-name TEXT,
-province TEXT
+"ID" TEXT PRIMARY KEY,
+"Categories" TEXT,
+"City" TEXT,
+"Restaurant_Name" TEXT,
+"State" TEXT
 );
+
 
 CREATE TABLE state_population (
 id INT PRIMARY KEY,
-state_name TEXT,
-state_abbr TEXT,
-population INT
+"State Abbreviation" TEXT,
+"State Name" TEXT,
+"Population" INT,
+"Percent of Total Population" FLOAT
 );
 
 
 --aggregate across states to get count of restaurants
 SELECT 
-	province,
-	count(name) restaurant_count
+	"State",
+	COUNT("Restaurant_Name") restaurant_count
 FROM restaurant
-GROUP BY province
-ORDER BY count(name) DESC;
+GROUP BY "State"
+ORDER BY COUNT("Restaurant_Name") DESC;
+
+
+--aggregate across states to get count of restaurants joined with populations
+SELECT 
+	r."State",
+	s."Population",
+	COUNT(r."Restaurant_Name") restaurant_count	
+FROM state_population s
+JOIN restaurant r
+	ON s."State Abbreviation" = r."State"
+GROUP BY r."State", s."Population"
+ORDER BY s."Population" DESC;
+
+
+--number of restaurants in the top 15 most populous states
+SELECT 
+	r."State",
+	s."Population",
+	COUNT(r."Restaurant_Name") restaurant_count	
+FROM state_population s
+JOIN restaurant r
+	ON s."State Abbreviation" = r."State"
+GROUP BY r."State", s."Population"
+ORDER BY s."Population" DESC 
+LIMIT 15;
+
+
